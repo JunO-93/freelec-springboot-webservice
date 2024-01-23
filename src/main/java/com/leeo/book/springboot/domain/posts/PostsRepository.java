42929cb8da66e9d935c79c6a132050b75d7df3d3
JunO-93 <dlwnsdh17@naver.com>
2024-01-23 22:55:05 +0900

    전체데이터 조회기능 추가
    Signed-off-by: JunO-93 <dlwnsdh17@naver.com>

--- src/main/java/com/leeo/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/leeo/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,12 @@
 package com.leeo.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long>{
     //MyBatis Dao 역할
-
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/leeo/book/springboot/web/IndexController.java
+++ src/main/java/com/leeo/book/springboot/web/IndexController.java
@@ -1,17 +1,26 @@
 package com.leeo.book.springboot.web;
 
+import com.leeo.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
+
+@RequiredArgsConstructor // 머선 어노테이션인지 내일 정리하자
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
     }
+
 }
